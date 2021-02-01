036d73f9674ebf6fc74aaa334961a8da98db6eeb
Jay P <jason@notreal.com>
2021-02-01 10:26:35 -0500

    Inital Java Spring project

---
+++ src/main/java/com/portjm1221/connectthedots/ConnectthedotsApplication.java
@@ -0,0 +1,13 @@
+package com.portjm1221.connectthedots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectthedotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectthedotsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portjm1221/connectthedots/ConnectthedotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portjm1221.connectthedots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectthedotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
